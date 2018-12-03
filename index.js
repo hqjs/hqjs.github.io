d997b9a0a2af02dff09bf53f820bb5af835e4e16
hqjs <hqjsorg@gmail.com>
2018-12-03 21:04:33 +0100

    Improve error handler

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Logo from '/logo.js';
 
 (async () => {
-    const res = await fetch('/logo.json');
-    const logo = await res.json();
-    const logoEl = document.querySelector('.js-logo');
+    if (!window.error) {
+        const res = await fetch('/logo.json');
+        const logo = await res.json();
+        const logoEl = document.querySelector('.js-logo');
 
-    new Logo(logoEl, logo);
+        new Logo(logoEl, logo);
+    }
 })();
